ee23dd27b6baf6b41703a6753d27303056067e74
Danny <Danny.nguyen5316@gmail.com>
2019-08-24 17:40:29 -0500

    The Odin Project JavaScript exercises: helloWorld, repeatString, reverseString. Working: removeFromArray

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
 const removeFromArray = function() {
-
-}
-
-module.exports = removeFromArray
+  let myArray = arguments[0];
+  for (var i = 0; i < myArray.length+1; i++) {
+    for (var j = 1; j < arguments.length; j++) {
+      if (arguments[j] === myArray[i]) {
+        myArray.splice(i, 1);
+      }
+    }
+  }
+  return myArray;
+};
+module.exports = removeFromArray;
