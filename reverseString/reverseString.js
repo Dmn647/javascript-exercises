945fd6db2897aae33854954a12fa69b46675fb53
Danny <Danny.nguyen5316@gmail.com>
2019-08-24 17:52:59 -0500

    initial commit

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function(strng) {
+  let result = "";
+  for (let i = strng.length - 1; i >= 0; i--) {
+    result += strng[i];
+  }
+  return result;
+};
 
-}
-
-module.exports = reverseString
+module.exports = reverseString;
