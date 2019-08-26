a2aa4269bb3477ba2d776ea436c3732d2bca158c
Danny <Danny.nguyen5316@gmail.com>
2019-08-26 12:56:26 -0500

    Clean solution

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,13 +1,9 @@
 const ftoc = function(temp) {
-  let celsius = (temp - 32) * (5 / 9);
-  let rounded = Math.round(celsius * 10) / 10;
-  return rounded;
+  return Math.round((temp - 32) * (5 / 9) * 10) / 10;
 };
 
 const ctof = function(temp) {
-  let fahrenheit = temp * (9 / 5) + 32;
-  let rounded = Math.round(fahrenheit * 10) / 10;
-  return rounded;
+  return Math.round((temp * (9 / 5) + 32) * 10) / 10;
 };
 
 module.exports = {
